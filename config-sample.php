1d3fdd0f3a4783bd1939434d39cf2d9ee64093ec
pxgamer <owzie123@gmail.com>
2018-11-15 23:06:08 +0000

    Move config to be a sample

---
+++ config-sample.php
@@ -0,0 +1,29 @@
+<?php
+
+// The information used to set up the notification system
+
+return [
+    // The name of the server you are connecting to, usually localhost.
+    'server_name' => 'localhost',
+
+    // The name of the database to connect to.
+    'db_name'     => '',
+
+    // The username you connect to the database with.
+    'db_user'     => '',
+
+    // The password you connect to the database with.
+    'db_pass'     => '',
+
+    // The email that you wish to receive emails at.
+    'mail_to'     => '',
+
+    // The email you wish to be notified from.
+    'mail_from'   => '',
+
+    // The IP of the masternode.
+    'ip'          => '',
+
+    // The public key, used to return the masternode balance when a block is won.
+    'public_key'  => '',
+];
